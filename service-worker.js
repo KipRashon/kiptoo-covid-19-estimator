a6ad1ea3daefc20a5dd0364a4546c92534c596ee
KipRashon <46610863+KipRashon@users.noreply.github.com>
2020-04-13 11:15:01 -0700

    manifest

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,61 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
-'use strict';
+var CACHE_NAME = 'kiptoo-covid-19-estimator';
+var urlsToCache = [
+    '/',
+    '/src/css/bootstrap.min.css'
+]
 
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
-
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-];
-
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-
-  self.skipWaiting();
+self.addEventListener('install',function(event){
+    //the installation steps 
+   event.waitUntil(
+       caches.open(CACHE_NAME)
+       .then(function(cache) {
+           console.log('Opened Cache');
+           return cache.addAll(urlsToCache);
+       })
+   )
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
+self.addEventListener('fetch',function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if(response){return response;}
+            return fetch(event.request).then(
+                function (response) {
+                    //Check if we recieved a valid response 
+                    if(!response || response.status !==200 || response.type !=='basic'){
+                        return response;
+                    }
 
-  self.clients.claim();
-});
+                    var responseToCache = response.clone();
 
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
+                    caches.open(CACHE_NAME)
+                    .then(function(cache){
+                        cache.put(event.request,responseToCache);
+                    });
 
-});
+                    return response;
+                }
+            )
+        }
+        
+        )
+    )
+})
+
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+  
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
